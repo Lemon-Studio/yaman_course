c3a1ac0ad098e9737345d2d7206f58c1e67a802f
Murilo Machado <murilomachado@Murilos-MacBook-Pro.local>
2017-01-30 17:49:39 -0200

    Vinicius - Continuacao testes

--- treinamento_ios/features/example.feature
+++ treinamento_ios/features/example.feature
@@ -4,5 +4,6 @@ Funcionalidade: Acesso as telas do sistema
   Cenário: Alternar entre telas do sistema
 
     Dado que o app "IOS" foi executado com sucesso
-    E eu preencho o campo "Nome"
-    E eu preencho o campo "Nome"
+    E eu preencho o campo "text" com o valor "nome"
+    E eu preencho o campo "numeric" com o valor "numero"
+    Então eu ativo o campo "tab-bar-scrolling"
--- treinamento_ios/features/step_definitions/step_definitions.rb
+++ treinamento_ios/features/step_definitions/step_definitions.rb
@@ -2,8 +2,12 @@ Dado(/^que o app "([^"]*)" foi executado com sucesso$/) do |arg1|
   wait_for_none_animating
 end
 
-Dado(/^eu preencho o campo "([^"]*)"$/) do |arg1|
-  wait_for_element_exists("* id:'text'")
-  touch("* id:'text'")
-  keyboard_enter_text(arg1)
+Dado(/^eu preencho o campo "([^"]*)" com o valor "([^"]*)"$/) do |arg1, arg2|
+  wait_for_element_exists("* id:'#{arg1}'")
+  touch("* id:'#{arg1}'")
+  keyboard_enter_text(arg2)
+end
+
+Dado(/^eu ativo o campo "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
 end
